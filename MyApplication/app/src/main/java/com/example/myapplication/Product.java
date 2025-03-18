cad430d2d320caccf19482b634a2419104735dc6
Utshav1120 <lamautshav55555@gmail.com>
2025-03-18 07:50:30 +0545

    IDK what i did

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Product.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+public class Product {
+    private String name ;
+    private String description ;
+    private double price ;
+    private int  image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public CharSequence getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
